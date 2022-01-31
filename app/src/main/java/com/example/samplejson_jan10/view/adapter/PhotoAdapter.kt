542261d90409630b42d88c7d15fc3589ce2eebae
tduringer <94665592+tduringer@users.noreply.github.com>
2022-01-31 18:55:13 -0500

    Glide implementation working. The main problem is that the url and thumbnail data don't have a file extension. I gave them both a .png extension and viola it works.

--- app/src/main/java/com/example/samplejson_jan10/view/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/samplejson_jan10/view/adapter/PhotoAdapter.kt
@@ -1,10 +1,10 @@
 package com.example.samplejson_jan10.view.adapter
 
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.samplejson_jan10.databinding.FragmentTodoBinding
 import com.example.samplejson_jan10.databinding.RowItemPhotoBinding
 import com.example.samplejson_jan10.model.network.models.Photo
 
@@ -43,12 +43,25 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
             tvUrl.text = photo.url
             tvThumbnail.text = photo.thumbnailUrl
 
+
+//            val urlTestString = "https://static.vecteezy.com/system/resources/previews/000/450/367/original/android-vector-icon.jpg"
+//            val urlTestString2 = "https://via.placeholder.com/600/92c952"
+//            val urlTestString3 = "https://purepng.com/public/uploads/large/51500927739gw8errqafm7fj1ftscuml1qd1msydpvtrrdhxct8a89hkc0rkarovqknvmrye9i08o5zoop1tgh71bslw4bio58a7rxz2tlkntvr.png"
+//            val urlTestString4 = "https://via.placeholder.com/600/92c952.png"
+
+            Glide.with(ivUrl.context).load(photo.url + ".png").into(ivUrl)
+            Glide.with(ivThumbnail.context).load(photo.thumbnailUrl + ".png").into(ivThumbnail)
+            //note that the url passed from the api is not working but this urlTestString is
+//            Glide.with(ivUrl.context).load(urlTestString).into(ivUrl)
+//            Glide.with(ivUrl.context).load(urlTestString).into(ivThumbnail)
 //            ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
 //
 //            Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
 
-            Glide.with(ivUrl.context).load(photo.url).into(ivUrl)
-            Glide.with(ivThumbnail.context).load(photo.thumbnailUrl).into(ivThumbnail)
+//              AppGlideModule
+            //MyAppGlideModule.with(ivUrl.context).load(photo.url).into(ivUrl)
+//            Glide.with(ivUrl.context).load(photo.url).into(ivUrl)
+//            Glide.with(ivThumbnail.context).load(photo.thumbnailUrl).into(ivThumbnail)
 
             // implement url and thumbnail with glide later
 
